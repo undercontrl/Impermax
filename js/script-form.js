1ca9fc34951b2167c0c66ce55403ae8e4b79e975
Ariane-dev <ariane.rosa500@gmail.com>
2025-08-05 11:57:35 -0300

    adicionado o java

---
+++ js/script-form.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const PUBLIC_KEY = 'opM4tQeS5gzjrwzVK'
+    const YOUR_SERVICE_I = 'service_8efgeqf'
+    const YOUR_TEMPLATE_ID = 'template_pcqbe2p';
+    emailjs.init(PUBLIC_KEY);
+    document.getElementById('form-contato').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const statusMessage = document.getElementById('status-mensagem');
+        const submitButton = this.querySelector('button[type="submit"]');
+
+        submitButton.textContent = 'Enviando...';
+        submitButton.disabled = true;
+        statusMessage.textContent = '';
+
+        const servico = document.getElementById('servico').value
+        const telefone = document.getElementById('telefone').value
+
+        const menssage = `Telefone: ${telefone}  -  Servico: ${servico} `
+        const template = {
+            name: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            message: menssage
+        };
+
+        //console.log(template)
+        emailjs.send(YOUR_SERVICE_I, YOUR_TEMPLATE_ID, template)
+            .then(function(response) {
+                console.log('SUCESSO!', response.status, response.text);
+                statusMessage.textContent = 'E-mail enviado com sucesso!';
+                statusMessage.style.color = 'green';
+                
+                document.getElementById('form-contato').reset();
+
+            }, function(error) {
+                console.log('FALHA...', error);
+                statusMessage.textContent = 'Falha ao enviar o e-mail. Tente novamente mais tarde.';
+                statusMessage.style.color = 'red';
+            })
+            .finally(function() {
+                submitButton.textContent = 'Enviar Mensagem';
+                submitButton.disabled = false;
+            });
+    });
+});
