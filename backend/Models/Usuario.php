a4e92beac6cea2cb7a48826574ab24d5b8c4f973
undercontrl <lumaiza@gmail.com>
2025-12-02 11:27:29 -0300

    asdasdas

--- backend/Models/Usuario.php
+++ backend/Models/Usuario.php
@@ -437,4 +437,22 @@ class Usuario{
         return $statement->execute($params);
     }
 
+    public function paginacaoAPI(int $pagina = 1, int $por_pagina = 10): array{
+        $totalQuery = "SELECT COUNT(*) FROM `tbl_usuario`";
+        $totalStmt = $this->db->query($totalQuery);
+        $total_de_registros = $totalStmt->fetchColumn();
+        $offset = ($pagina - 1) * $por_pagina;
+        $dataQuery = "SELECT * FROM `tbl_usuario` LIMIT :limit OFFSET :offset";
+        $dataStmt = $this->db->prepare($dataQuery);
+        $dataStmt->bindValue(':limit', $por_pagina, PDO::PARAM_INT);
+        $dataStmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $dataStmt->execute();
+        $dados = $dataStmt->fetchAll(PDO::FETCH_ASSOC);
+        $lastPage = ceil($total_de_registros / $por_pagina);
+
+        return [
+            'data' => $dados
+        ];
+    }
+
 }
